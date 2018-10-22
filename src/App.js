ec1c2ca130c2c37aefd65707b58127379c5ce122
Thomas Sameshima <thomasryusameshima@gmail.com>
2018-10-22 18:03:15 -0300

    Getting pet data from the api

--- src/App.js
+++ src/App.js
@@ -31,16 +31,67 @@ breed: "Siamese"
 
 import React from 'react';
 import { render } from 'react-dom'; // will only pull the 'render' function from ReactDOM
+import pf from 'petfinder-client';
 import Pet from './Pet';
+
+const petfinder = pf({
+    key: process.env.API_KEY,
+    secret: process.env.API_SECRET
+});
 
 // Class component
 class App extends React.Component {
-    handleTitleClick() {
-        alert('You clicked the title');
+    // First thing called when the component is created
+    constructor(props) {
+        super(props); // Calls React.Component's constructor, passing props to it (obligatory)
+
+        this.state = {
+            pets: [] // Defines the default/initial state of the component
+        };
+    }
+
+    // Called when the component's initial render is finished
+    componentDidMount() {
+        /* 
+        const promise = petfinder.breed.list({ animal: 'dog' }); // Simple promise usage
+        promise.then(console.log, console.error);
+        */
+
+        petfinder.pet
+            .find({ output: 'full', location: 'Seattle, WA' }) // Pet available for adoption in Seattle
+            .then(data => {
+                let pets;
+
+                // Simple verifications so pets is always an Array
+                if (data.petfinder.pets && data.petfinder.pets.pet) {
+                    if (Array.isArray(data.petfinder.pets.pet)) {
+                        pets = data.petfinder.pets.pet;
+                    } else {
+                        pets = [data.petfinder.pets.pet];
+                    }
+                } else {
+                    pets = [];
+                }
+
+                /*
+                this.state.pets = pets; // This does not work, React has to know when pets' value change
+                */
+
+                // For the setState object, we pass an object of things to update, so we only define as 'updated'
+                // things that were truly changed/updated, leaving all the other state values untouched
+                this.setState({
+                    pets // same as 'pets: pets' (first pet being state's pet and the second, the let's)
+                });
+            });
     }
 
     render() {
-        /* return React.createElement('div', {}, [
+        /* 
+        handleTitleClick() {
+            alert('You clicked the title');
+        }
+        
+        return React.createElement('div', {}, [
             React.createElement(
                 'h1',
                 { onClick: this.handleTitleClick }, // 'this' refers to the App instance
@@ -61,14 +112,49 @@ class App extends React.Component {
                 animal: 'cat',
                 breed: 'Siamese'
             })
-        ]); */
+        ]);
+        */
+
+        /*
+        return ( 
+            <pre>
+                <code>{JSON.stringify(this.state, null, 4)}</code> // Print the current state's contents
+            </pre>
+
+            <h1>Adopt Me!</h1>
+
+            <Pet name="Luna" animal="dog" breed="Havanese" />
+            <Pet name="Pepper" animal="bird" breed="Cockatiel" /> // Not using API/state to show pets
+            <Pet name="Doink" animal="cat" breed="Siamese" />
+        );
+        */
 
         return (
             <div>
                 <h1>Adopt Me!</h1>
-                <Pet name="Luna" animal="dog" breed="Havanese" />
-                <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-                <Pet name="Doink" animal="cat" breed="Siamese" />
+
+                <div>
+                    {this.state.pets.map(pet => {
+                        let breed;
+
+                        if (Array.isArray(pet.breeds.breed)) {
+                            breed = pet.breeds.breed.join(', '); // Pets can have multiple breeds
+                        } else {
+                            breed = pet.breeds.breed;
+                        }
+
+                        return (
+                            <Pet
+                                key={pet.id}
+                                animal={pet.animal}
+                                name={pet.name}
+                                breed={breed}
+                                media={pet.media}
+                                location={`${pet.contact.city}, ${pet.contact.state}`}
+                            />
+                        );
+                    })}
+                </div>
             </div>
         );
     }
--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+/* 
 // Function component
 const Pet = props => {
+
     // no JSX
-    /* return React.createElement('div', {}, [
+    return React.createElement('div', {}, [
         React.createElement('h1', {}, props.name),
         React.createElement('h2', {}, props.animal),
         React.createElement('h2', {}, props.breed)
-    ]); */
+    ]);
 
     // with JSX
     return (
@@ -18,5 +20,35 @@ const Pet = props => {
         </div>
     ); // parenthesis allows us to continue the code in the following line(s)
 };
+*/
+
+class Pet extends React.Component {
+    render() {
+        // Destructuring, basically the paremeters required from
+        // the parent for this component to work
+        const { name, animal, breed, media, location } = this.props;
+
+        let photos = [];
+        if (media && media.photos && media.photos.photo) {
+            photos = media.photos.photo.filter(
+                photo => photo['@size'] === 'pn' // We only want one of the photo sizes
+            );
+        }
+
+        return (
+            <div className="pet">
+                <div className="image-container">
+                    <img alt={'photo of' + name} src={photos[0].value} />
+                </div>
+                <div className="info">
+                    <h1>{name}</h1>
+                    <h2>
+                        {animal} - {breed} - {location}
+                    </h2>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Pet;
